dd981434e84bb2b5c20f1be2c432645f3340551d
MJ34 <kasur.rusak404@gmail.com>
2022-12-31 20:12:14 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaWaliController;
+use App\Http\Controllers\BerandaOperatorController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('operator')->middleware(['auth', 'auth.operator'])->group(function () {
+    //ini route khusus untuk operator
+    Route::get('beranda', [BerandaOperatorController::class, 'index'])->name('operator.beranda');
+});
+
+Route::prefix('wali')->middleware(['auth', 'auth.wali'])->group(function () {
+    //ini route khusus untuk wali-murid
+    Route::get('beranda', [BerandaWaliController::class, 'index'])->name('wali.beranda');
+});
+
+Route::prefix('admin')->middleware(['auth', 'auth.admin'])->group(function () {
+    //ini route khusus untuk admin
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('logout', function () {
+    Auth::user()->logout();
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
