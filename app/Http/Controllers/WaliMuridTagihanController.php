85dea4f4d8bd2ac8f9e775bc69e5deb1b208d5c1
kasur.rusak404@gmail.com <1631Nz94@>
2023-03-09 15:29:43 +0700

    Membuat Form Informasi Tagihan Wali Murid

--- app/Http/Controllers/WaliMuridTagihanController.php
+++ app/Http/Controllers/WaliMuridTagihanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BankSekolah;
 use App\Models\Tagihan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -19,6 +20,7 @@ class WaliMuridTagihanController extends Controller
     public function show($id)
     {
         $tagihan = Tagihan::find($id);
+        $data['bankSekolah'] = BankSekolah::all();
         $data['tagihan'] = $tagihan;
         $data['siswa'] = $tagihan->siswa;
         return view('wali.tagihan_show', $data);
--- resources/views/wali/tagihan_show.blade.php
+++ resources/views/wali/tagihan_show.blade.php
@@ -30,12 +30,39 @@
                             </tr>
                         </tfoot>
                     </table>
-                    <div class="alert alert-secondary mt-4" role="alert">
-                        Pembayaran Bisa dilakukan dengan cara langsung ke Operator sekolah atau di transfer melalui bank
-                        milik sekolah berikut: <br />
-                        <ul>
-                            
-                        </ul>
+                    <div class="alert alert-secondary mt-4" role="alert" style="color: black">
+                        Pembayaran Bisa dilakukan dengan cara langsung ke Operator sekolah atau di transfer melalui rekening milik sekolah ini. <br />
+                        Jangan Melakukan transfer ke rekening selain Rekening dibawah ini
+                    </div>
+                    <ul>
+                        <li><a href="http://">Lihat Cara Pembayaran Melalui ATM</a></li>
+                        <li><a href="http://">Lihat Cara Pembayaran Melalui internet Banking</a></li>
+                    </ul>
+                    Setelah melakukan pembayaran, silahkan upoload bukti pembayaran melalui tombol konfirmasi yang ada di bawah ini: 
+                    <div class="row">
+                        @foreach ($bankSekolah as $itemBank)
+                        <div class="col-md-6">
+                            <div class="alert alert-info" role="alert">
+                                <table width="100%">
+                                    <tbody>
+                                        <tr>
+                                            <td width="40%">Bank Tujuan</td>
+                                            <td>: {{ $itemBank->nama_bank }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td width="40%">Nomor Rekening</td>
+                                            <td>: {{ $itemBank->nomor_rekening }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td width="40%">Atas Nama</td>
+                                            <td>: {{ $itemBank->nama_rekening }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <a href="" class="btn btn-primary btn-sm mt-3">Konfirmasi Pembayaran</a>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
